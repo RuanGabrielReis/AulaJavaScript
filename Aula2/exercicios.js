4e9982f204bc91b5e9cb0d0ed78fb5e78cc50220
Ruan <ruangabreis28@gmail.com>
2025-03-20 10:53:50 -0300

    20/03

--- Aula2/exercicios.js
+++ Aula2/exercicios.js
@@ -7,3 +7,18 @@ document.getElementById("quadrado").innerHTML = "o quadrado do numero é: " + qu
 document.getElementById("quadrado").innerHTML = "o quadrado do numero é: " + quadrado;
 
 }
+
+function ex6(){
+    let qtde = document.getElementById("qtde").value;
+    let preco = document.getElementById("preco").value;
+    
+    let subtotal = qtde*preco;
+    let desconto = subtotal*0.1;
+    let valorFinal = subtotal-desconto;
+
+    document.getElementById("subtotal").innerHTML = "O subtotal é de: " + subtotal;
+    document.getElementById("desconto").innerHTML = "O desconto é de: " + desconto;
+    document.getElementById("valorFinal").innerHTML = "O valor final é de: " + valorFinal;
+
+}
+    
