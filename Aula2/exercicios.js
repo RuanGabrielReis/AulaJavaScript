5953018dceee44f6f27e952fab429af981ef261c
Ruan <ruangabreis28@gmail.com>
2025-03-20 11:04:35 -0300

    20/03

--- Aula2/exercicios.js
+++ Aula2/exercicios.js
@@ -22,3 +22,12 @@ function ex6(){
 
 }
     
+function calcularMedia() {
+    var nota1 = parseFloat(document.getElementById('nota1').value);
+    var nota2 = parseFloat(document.getElementById('nota2').value);
+    var nota3 = parseFloat(document.getElementById('nota3').value);
+
+    var media = (nota1 + nota2 + nota3) / 3;
+
+    document.getElementById('media').innerText = 'A média aritmética é: ' + media.toFixed(2);
+}
