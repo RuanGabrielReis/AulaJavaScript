0a115fdcdd9cfef2547c2648b9a27abb58286c8e
Ruan <ruangabreis28@gmail.com>
2025-03-20 10:20:52 -0300

    20/03

--- Aula2/exemplo1.js
+++ Aula2/exemplo1.js
@@ -1,20 +1,24 @@
-let numero1,numero2,resultado;
+let numero1, numero2, resultado;
 
-function somar(n1,n2) {
+function somar(n1,n2){
     let soma = Number(n1) + Number(n2);
-    document.getElementById("mensagem").innerHTML = "A soma dos números é: " + soma;
+    document.getElementById("mensagem").innerHTML = "a soma é: " + soma;
 }
 
-function solicitarEntradaDados() {
-    numero1 = Number(prompt("Digite o número 1: "));
-    numero2 = Number(prompt("Digite o número 2: "));
+//somar() ;
+
+function solicitarEntradaDados(){
+    numero1 = Number(prompt("digite o primeiro numero: "));
     
-    somar(numero1,numero2);
+    numero2 = Number(prompt("digite o segundo numero: "));
+ somar(numero1,numero2);
 }
 
-function processar() {
+function processar(){
     numero1 = document.getElementById("numero1").value;
     numero2 = document.getElementById("numero2").value;
-    
-    somar(numero1,numero2);
+
+    somar(numero1, numero2);
+
 }
+
--- Aula2/exercicios.js
+++ Aula2/exercicios.js
@@ -1,32 +1,9 @@
-function ex1() {
-    let numero = document.getElementById("numero").value;
+function ex1(n1){
+let quadrado = Number(n1)**2;
+let numero = Number(n1);
+let cubo = Number(n1)**3;
+document.getElementById("quadrado").innerHTML = "o quadrado do numero é: " + quadrado;
+document.getElementById("quadrado").innerHTML = "o quadrado do numero é: " + quadrado;
+document.getElementById("quadrado").innerHTML = "o quadrado do numero é: " + quadrado;
 
-    let quad = numero**2;
-    let cubo = numero**3;
-
-    
-    document.getElementById("numdig").innerHTML = "O numero digitado foi " + numero;
-    document.getElementById("quad").innerHTML = "O quadrado do numero é " + quad;
-    document.getElementById("cubo").innerHTML = "A cubo do numero é " + cubo;
-
-}
-
-function ex2() {
-    let fahren = document.getElementById("fahren").value;
-
-    let celsius = (fahren-32)*5/9;
-
-    document.getElementById("fahrenResult").innerHTML = "A temperatura em Fahrenheit foi " + fahren;
-    document.getElementById("celsiusResult").innerHTML = "A temperatura em Celsius é " + celsius;
-}
-
-function ex3() {
-    let prestacao = document.getElementById("prestacao").value;
-    let juros = document.getElementById("juros").value;
-    let atraso = document.getElementById("atraso").value;
-
-    let valorAtt = prestacao + (prestacao*(juros**2/100)*atraso);
-
-    document.getElementById("valoresInf").innerHTML = "Valores informados:";
-    document.getElementById("celsiusResult").innerHTML = "A temperatura em Celsius é " + celsius;
 }
